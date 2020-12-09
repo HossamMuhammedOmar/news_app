4986d4e370acd8615a85b2021cebbd7170eb5f7d
Your Name <hossamomar9876@gmail.com>
2020-12-09 19:20:16 +0200

    fetchTopIds and fetchItems

--- lib/src/resources/news_api_provider.dart
+++ lib/src/resources/news_api_provider.dart
@@ -1,10 +1,23 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' show Client;
+import 'package:news/src/models/item_model.dart';
+
+final _url = "https://hacker-news.firebaseio.com/v0";
 
 class NewsApiProvider {
   Client client = new Client();
-  fetchTopIds() {
-    client.get("https://hacker-news.firebaseio.com/v0/topstories.json");
+
+  fetchTopIds() async {
+    final response = await client.get("$_url/topstories.json");
+    final ids = json.decode(response.body);
+    return ids;
   }
 
-  fetchItems() {}
+  fetchItems(int id) async {
+    final response = await client.get("$_url/item/$id.json");
+    final parsedJson = json.decode(response.body);
+    return ItemModel.fromJson(parsedJson);
+  }
 }
+// 2532
