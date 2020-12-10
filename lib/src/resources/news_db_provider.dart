155a3b01b93028669e2902ff961b110cbd784989
Your Name <hossamomar9876@gmail.com>
2020-12-10 17:48:52 +0200

    init news_db_provider

---
+++ lib/src/resources/news_db_provider.dart
@@ -0,0 +1,6 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart';
+import 'dart:io';
+import 'dart:async';
+import '../models/item_model.dart';
